ab694231694a4eec2eb9cc374361e4cf1176bed2
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-05-26 15:24:45 -0600

    reserve

--- ReserveMe/resources/views/livewire/fast-booking.blade.php
+++ ReserveMe/resources/views/livewire/fast-booking.blade.php
@@ -1,5 +1,5 @@
 <div x-cloak x-data="{ open: false }" class="fixed bottom-0 right-5 text-cyan-50 z-50">
-    <div :class="{ 'h-[17.25vw]': open, 'h-12':!open }" class=" p-2 bg-red-700 overflow-hidden">
+    <div :class="{ 'h-[17.25vw]': open, 'h-12': !open }" class="p-2 bg-red-700 overflow-hidden transition-all duration-300">
         <div @click="open = !open"
             class="flex gap-2 cursor-pointer justify-center p-5 h-10 items-center bg-emerald-600 rounded-t-3xl flex-shrink-0 border border-black">
             <div class="text-xl whitespace-nowrap">Haz una reservación</div>
@@ -10,9 +10,8 @@
                     stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
             </svg>
         </div>
-        <div>
-            
+        <div class="flex gap-2 cursor-pointer justify-center p-5 h-10 items-center bg-emerald-600 rounded-b-3xl flex-shrink-0 border border-t-0 border-black">
+            ff
         </div>
-        
     </div>
 </div>
