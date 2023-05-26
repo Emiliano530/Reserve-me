6eb70272dfd4958e6fc756c8f07327c71c67c06b
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-05-26 15:00:31 -0600

    reserve

--- ReserveMe/resources/views/dashboard.blade.php
+++ ReserveMe/resources/views/dashboard.blade.php
@@ -3,6 +3,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-indigo-900 overflow-hidden shadow-md rounded-3xl">
                 <x-welcome />
+                <x-welcome />
+                <x-welcome />
+                <x-welcome />
             </div>
         </div>
     </div>
